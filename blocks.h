2bc8ee10b0a67552cc7cdea58d00fffa3236f892
Evan Harvey <harveyem@utas.edu.au>
2020-03-02 16:24:29 +1100

    Debloat bar

--- blocks.h
+++ blocks.h
@@ -3,15 +3,19 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	/* {"", "cat /tmp/recordingicon",	0,	9}, */
 	/* {"",	"music",	0,	11}, */
-	{"",	"pacpackages",	0,	8},
-	{"",	"torrent",	20,	7},
-	{"",	"news",		0,	6},
-	{"",	"weather",	18000,	5},
-	{"",	"mailbox",	180,	12},
+	/* {"",	"pacpackages",	0,	8}, */
+	/* {"",	"torrent",	20,	7}, */
+	/* {"",	"news",		0,	6}, */
+	/* {"",	"mailbox",	180,	12}, */
+	/*{"",	"weather",	18000,	5},*/
+	{"",	"cpu",	5,	5},
+	{"",	"memory",	5,	5},
+	{"",	"disk",	18000,	5},
+	{"",	"internet",	5,	5},
 	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	0},
+	{"",	"battery",	10,	0},
 	{"",	"clock",	60,	0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+//sets delimiter between status commands. NULL character ('\0') means no delimeter.
 static char delim = '|';
